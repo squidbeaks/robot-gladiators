2c8b2084b8909464f9459222cd73bfca86bdd76a
Leah Russell <leahsigridrussell@gmail.com>
2021-02-15 11:46:35 -0800

    Add high score functionality using localStorage API

--- assets/js/game.js
+++ assets/js/game.js
@@ -102,12 +102,12 @@ var fightOrSkip = function() {
 var fight = function(enemy) {
     // keep track of who goes first
     var isPlayerTurn = true;
-  ​
+
     // randomly change turn order
     if (Math.random() > 0.5) {
       isPlayerTurn = false;
     }
-  ​
+
     while (playerInfo.health > 0 && enemy.health > 0) {
       if (isPlayerTurn) {
         // ask player if they'd like to fight or skip using fightOrSkip function
@@ -115,9 +115,9 @@ var fight = function(enemy) {
           // if true, leave fight by breaking loop
           break;
         }
-  ​
+
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-  ​
+
         // remove enemy's health by subtracting the amount we set in the damage variable
         enemy.health = Math.max(0, enemy.health - damage);
         console.log(
@@ -130,14 +130,14 @@ var fight = function(enemy) {
             enemy.health +
             " health remaining."
         );
-  ​
+
         // check enemy's health
         if (enemy.health <= 0) {
           window.alert(enemy.name + " has died!");
-  ​
+
           // award player money for winning
           playerInfo.money = playerInfo.money + 20;
-  ​
+
           // leave while() loop since enemy is dead
           break;
         } else {
@@ -146,7 +146,7 @@ var fight = function(enemy) {
         // player gets attacked first
       } else {
         var damage = randomNumber(enemy.attack - 3, enemy.attack);
-  ​
+
         // remove player's health by subtracting the amount we set in the damage variable
         playerInfo.health = Math.max(0, playerInfo.health - damage);
         console.log(
@@ -159,7 +159,7 @@ var fight = function(enemy) {
             playerInfo.health +
             " health remaining."
         );
-  ​
+
         // check player's health
         if (playerInfo.health <= 0) {
           window.alert(playerInfo.name + " has died!");
@@ -207,13 +207,25 @@ var startGame = function() {
 };
 
 var endGame = function() {
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var HighScore = localStorage.getItem("highscore");
+    if (HighScore === null) {
+        HighScore = 0;
+    }
+    // if player has more money than the high score, player has new high score
+    if (playerInfo.money > HighScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     }
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + HighScore + ". Maybe next time!");
     }
 
+    // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
